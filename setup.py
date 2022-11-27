dfdce761b215b01d35c4870588595e1a4adef1a6
Lincoln Stein <lincoln.stein@gmail.com>
2022-11-27 04:19:31 +0000

    this installs the Makefile into the distro during pip install
    - Also relaxes the python requirement to >=3.9

--- setup.py
+++ setup.py
@@ -11,11 +11,12 @@ requirements = [
 setup(
     name='pypatchmatch',
     packages=['patchmatch'],
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/invoke-ai/PyPatchMatch',
     python_requires='>=3.9',
     install_requires=requirements,
     description='This library implements the PatchMatch based inpainting algorithm.',
     long_description=readme,
     long_description_content_type="text/markdown",
+    package_data={'patchmatch': ['Makefile']},
 )
