14d2b7e0d0453bf45c9be2aa6c022840dbd3f1d0
Lincoln Stein <lincoln.stein@gmail.com>
2022-11-27 04:21:24 +0000

    bump version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 setup(
     name='pypatchmatch',
     packages=['patchmatch'],
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/invoke-ai/PyPatchMatch',
     python_requires='>=3.9',
     install_requires=requirements,
